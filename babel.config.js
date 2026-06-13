379c502c66ead37406195efdb5d4f69f10c9b221
Harshit <harshitjain1924@gmail.com>
2026-06-13 14:06:45 +0530

    chore: disable widgets and fix production reanimated crash

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,13 @@
 module.exports = function (api) {
   api.cache(true);
   const isTest = process.env.NODE_ENV === 'test';
+  const plugins = [];
+  if (isTest) {
+    plugins.push('dynamic-import-node');
+  }
+  plugins.push('react-native-reanimated/plugin');
   return {
     presets: ['babel-preset-expo'],
-    plugins: isTest ? ['dynamic-import-node'] : [],
+    plugins,
   };
 };
